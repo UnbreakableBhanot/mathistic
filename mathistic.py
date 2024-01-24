aea7e728e54fdb8f17ffa39c7ee1835e1a458954
Shubham Bhanot <157055209+UnbreakableBhanot@users.noreply.github.com>
2024-01-25 03:44:29 +0530

    Update mathistic.py
    Major update added sub and multiplication features to matrices
    with error handling

--- mathistic.py
+++ mathistic.py
@@ -16,20 +16,71 @@ class matrical:
             Matrix.append(user_input.split())
 
     def __init__(self):
-        self.A = []
-        self.__creator(self.A)
-        print("Matrices formed for {} ".format(self))
-        self.__show_matrix(self.A)
-    
+        try:
+            self.A = []
+            self.__creator(self.A)
+            print("Matrices formed for {} ".format(self))
+            self.__show_matrix(self.A)
+        except ValueError:
+            print("Please Enter correct Integer Value")
+
     def __show_matrix(self, other):
         for i in other:
             print(i)
     
     def __add__(self,other):
-        temp = []
-        for i in range(len(self.A)):
-            temp.append([])
-            for j in range(3):
-                temp[i].append(int(self.A[i][j]) + int(other.A[i][j]))
+        try:
+            temp = []
+            for i in range(len(self.A)):
+                temp.append([])
+                for j in range(3):
+                    temp[i].append(int(self.A[i][j]) + int(other.A[i][j]))
         
-        return temp
+            return temp
+    
+        except IndexError:
+            print("For addition Matrices should be of same size")
+
+        except ValueError:
+            print("Please Enter correct Integer value")
+
+    def __sub__(self,other):
+        try:
+            temp = []
+            for i in range(len(self.A)):
+                temp.append([])
+                for j in range(3):
+                    temp[i].append(int(self.A[i][j]) - int(other.A[i][j]))
+
+            return temp
+
+        except IndexError:
+            print("For Subtraction Matrices should be of same size")
+
+        except ValueError:
+            print("Please Insert correct integer value")
+            
+
+    def __mul__(self,other):
+        try:
+            temp = None
+            if len(self.A[0]) == len(other.A) and len(self.A) == len(other.A[0]):
+                temp = []
+            
+                for i in range(len(self.A)):
+                    temp.append([])
+                    temp_left_add = 0
+                    temp_right_add = 0
+                    for j in range(len(other.A[0])+1):
+                        temp_left_add += (int(self.A[i][j])*int(other.A[j][0]))
+                        temp_right_add += (int(self.A[i][j])*int(other.A[j][1]))
+
+                    temp[i].append(temp_left_add)
+                    temp[i].append(temp_right_add)
+                    
+            else:
+                print("multiplication is not possible")
+                
+            return temp
+        except ValueError:
+            print("Please Enter correct Integer Value")
